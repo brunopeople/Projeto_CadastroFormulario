87a0a2c5f79d9cb205e22518120c8d677e1aec4c
brunopeople <brunopessoa.melo@gmail.com>
2018-11-27 15:27:05 -0300

    subindo o projeto no repositório

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,10 @@
+<?php namespace formulario\Http\Controllers; 
+
+use app\User;
+use  app\Http\Controllers\Controller;
+
+class UsuariosController{
+	public function listaUsuarios(){
+		return '<h1>Lista de Usuarios</h1>';
+	}
+}
